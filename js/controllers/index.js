461b39c0feedd3f39e815662e540326678913f8e
MyCah <mycahp@gmail.com>
2017-07-04 22:37:44 -0400

    Test Ghost API

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -2,7 +2,17 @@ angular
     .module('app')
     .controller('IndexController', IndexController);
 
-function IndexController() {
+function IndexController($http) {
     var vm = this;
     vm.chosenSkill = "javascript";
+
+    ghost.init({
+        clientId: "ghost-frontend",
+        clientSecret: "cd0ea876ab55"
+    });
+
+    $http.get(ghost.url.api('posts', {limit: 3}))
+        .then(function(response) {
+            console.log(response);
+    });
 }
