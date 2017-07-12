d72436e2aeca21b195832e6bf417c8aff542cfcf
MyCah Pleasant <mycahp@gmail.com>
2017-07-11 22:08:46 -0400

    Add Google analytics, submit to contact API

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -2,9 +2,12 @@ angular
     .module('app')
     .controller('IndexController', IndexController);
 
-function IndexController($http) {
+function IndexController($http, $httpParamSerializerJQLike) {
     var vm = this;
     vm.chosenSkill = "javascript";
+
+    vm.contactForm = {
+    };
 
     ghost.init({
         clientId: "ghost-frontend",
@@ -15,4 +18,27 @@ function IndexController($http) {
         .then(function(response) {
             vm.posts = response.data.posts;
         });
+
+    vm.submitContact = function(contactForm) {
+        console.log(contactForm);
+
+        vm.messageSent = false;
+        vm.errorSending = false;
+
+        $http({
+            url: 'https://contact.mycahpleasant.com',
+            method: 'POST',
+            data: $httpParamSerializerJQLike(contactForm),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function(response) {
+                vm.messageSent = true;
+                vm.contactForm = {};
+            }, function(error) {
+                console.log(error);
+                vm.errorSending = true;
+                vm.sendingError = error.data.error;
+            });
+    }
 }
